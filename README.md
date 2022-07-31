5cbe68dcee630d682deee8117b662adf118b7860
Anuj Parakh <anuj@anujinfotech.com>
2022-07-31 12:00:54 -0500

    Added working photography section

--- README.md
+++ README.md
@@ -1,5 +1,16 @@
 # dawn-janacek
 
+## Photography Section
+
+In order to add an image to the photography section, use the following steps:
+- Add a `jpeg` image to the folder [assets/theme/images/photography/](assets/theme/images/photography) in the format `photo-#.jpeg` replacing `#` with a number in order (1, 2, 3..)
+- The numbered photos in the above folder will be displayed in order
+- Go to the file [assets/theme/js/photography.js](assets/theme/js/photography.js)
+- Change the variable called `NUM_PHOTOS` at the top of the file to be the **highest numbered photo**
+- This should update the photography section with all the images from the photography assets folder.
+
+> Images must be named correctly and be in jpeg format
+
 ## Helpful Info
 
 - Website html code is all in `index.html`
--- assets/theme/js/photography.js
+++ assets/theme/js/photography.js
@@ -1,13 +1,27 @@
 // Change this to show a different number of images
-const NUM_PHOTOS = 5;
+const NUM_PHOTOS = 18;
 
 const SOURCE_START = "assets/theme/images/photography/photo-";
+const backdropDiv = document.getElementById("backdrop-photo");
+const backdropImg = document.getElementById("backdrop-photo-img");
+
+backdropDiv.onclick = () => { backdropDiv.hidden = true }
+
+function openPhoto(imgSrc) {
+    console.log("Opening photo")
+}
+
 function populatePhotos() {
     for(var i = 1; i <= NUM_PHOTOS; ++i)
     {
         var img = document.createElement("img");
-        img.src = SOURCE_START + i + ".jpeg"
-
+        const src = SOURCE_START + i + ".jpeg"
+        img.src = src
+        img.onclick = (event) => {
+            console.log("opening photo")
+            backdropDiv.hidden = false
+            backdropImg.src = event.target.src
+         }
         document.getElementById("photo-gallery").appendChild(img)
     }
 }
