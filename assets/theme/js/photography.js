f35aa09776372b556af444ef519aa11f426580bc
CodeCzech <89798843+CodeCzech@users.noreply.github.com>
2024-09-04 15:44:35 -0500

    Update photography.js to Don's

--- assets/theme/js/photography.js
+++ assets/theme/js/photography.js
@@ -1,5 +1,5 @@
 // Change this to show a different number of images
-const NUM_PHOTOS = 36;
+/*const NUM_PHOTOS = 36;
 
 const SOURCE_START = "assets/theme/images/photography/photo-";
 const backdropDiv = document.getElementById("backdrop-photo");
@@ -24,6 +24,46 @@ function populatePhotos() {
          }
         document.getElementById("photo-gallery").appendChild(img)
     }
+}
+
+populatePhotos()
+*/
+
+const NUM_PHOTOS = 100;
+
+const SOURCE_START = "assets/theme/images/photography/photo-";
+const backdropDiv = document.getElementById("backdrop-photo");
+const backdropImg = document.getElementById("backdrop-photo-img");
+
+backdropDiv.onclick = () => { backdropDiv.hidden = true }
+
+function openPhoto(imgSrc) {
+  console.log("Opening photo")
+}
+
+function checkImage(src) {
+  let retval = false;
+  var http = new XMLHttpRequest();
+  http.open('HEAD', src, false);
+  http.send();
+  return http.status != 404;
+}
+
+function populatePhotos() {
+  for (var i = 1; i <= NUM_PHOTOS; ++i) {
+    var img = document.createElement("img");
+    const src = SOURCE_START + i + ".jpg"
+    if (checkImage(src)) {
+      img.src = src
+      console.log("doing something with" + src);
+      img.onclick = (event) => {
+        console.log("opening photo")
+        backdropDiv.hidden = false
+        backdropImg.src = event.target.src
+      }
+      document.getElementById("photo-gallery").appendChild(img)
+    }
+  }
 }
 
 populatePhotos()
